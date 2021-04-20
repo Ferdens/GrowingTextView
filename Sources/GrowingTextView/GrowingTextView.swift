bbaf3821000851b6a83c80596181c9f3be09818d
Anton Shepetuha <antonshepetuha@gmail.com>
2021-04-20 10:32:24 +0300

    palceholder name changed V2

--- Sources/GrowingTextView/GrowingTextView.swift
+++ Sources/GrowingTextView/GrowingTextView.swift
@@ -1,31 +1,18 @@
-//
-//  GrowingTextView.swift
-//  Pods
-//
-//  Created by Kenneth Tsang on 17/2/2016.
-//  Copyright (c) 2016 Kenneth Tsang. All rights reserved.
-//
-
 import Foundation
 import UIKit
-
 @objc public protocol GrowingTextViewDelegate: UITextViewDelegate {
     @objc optional func textViewDidChangeHeight(_ textView: GrowingTextView, height: CGFloat)
 }
-
 @IBDesignable @objc
 open class GrowingTextView: UITextView {
     override open var text: String! {
         didSet { setNeedsDisplay() }
     }
     private var heightConstraint: NSLayoutConstraint?
-    
     // Maximum length of text. 0 means no limit.
     @IBInspectable open var maxLength: Int = 0
-    
     // Trim white space and newline characters when end editing. Default is true
     @IBInspectable open var trimWhiteSpaceWhenEndEditing: Bool = true
-    
     // Customization
     @IBInspectable open var minHeight: CGFloat = 0 {
         didSet { forceLayoutSubviews() }
@@ -42,33 +29,27 @@ open class GrowingTextView: UITextView {
     @IBInspectable open var attributedPlaceholder: NSAttributedString? {
         didSet { setNeedsDisplay() }
     }
-    
     // Initialize
     override public init(frame: CGRect, textContainer: NSTextContainer?) {
         super.init(frame: frame, textContainer: textContainer)
         commonInit()
     }
-    
     required public init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         commonInit()
     }
-    
     private func commonInit() {
         contentMode = .redraw
         associateConstraints()
         NotificationCenter.default.addObserver(self, selector: #selector(textDidChange), name: UITextView.textDidChangeNotification, object: self)
         NotificationCenter.default.addObserver(self, selector: #selector(textDidEndEditing), name: UITextView.textDidEndEditingNotification, object: self)
     }
-    
     deinit {
         NotificationCenter.default.removeObserver(self)
     }
-    
     open override var intrinsicContentSize: CGSize {
         return CGSize(width: UIView.noIntrinsicMetric, height: 30)
     }
-    
     private func associateConstraints() {
         // iterate through all text view's constraints and identify
         // height,from: https://github.com/legranddamien/MBAutoGrowingTextView
@@ -80,40 +61,31 @@ open class GrowingTextView: UITextView {
             }
         }
     }
-    
     // Calculate and adjust textview's height
     private var oldText: String = ""
     private var oldSize: CGSize = .zero
-    
     private func forceLayoutSubviews() {
         oldSize = .zero
         setNeedsLayout()
         layoutIfNeeded()
     }
-    
     private var shouldScrollAfterHeightChanged = false
     override open func layoutSubviews() {
         super.layoutSubviews()
-        
         if text == oldText && bounds.size == oldSize { return }
         oldText = text
         oldSize = bounds.size
-        
         let size = sizeThatFits(CGSize(width: bounds.size.width, height: CGFloat.greatestFiniteMagnitude))
         var height = size.height
-        
         // Constrain minimum height
         height = minHeight > 0 ? max(height, minHeight) : height
-        
         // Constrain maximum height
         height = maxHeight > 0 ? min(height, maxHeight) : height
-        
         // Add height constraint if it is not found
         if (heightConstraint == nil) {
             heightConstraint = NSLayoutConstraint(item: self, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: height)
             addConstraint(heightConstraint!)
         }
-        
         // Update height constraint if needed
         if height != heightConstraint!.constant {
             shouldScrollAfterHeightChanged = true
@@ -126,7 +98,6 @@ open class GrowingTextView: UITextView {
             scrollToCorrectPosition()
         }
     }
-    
     private func scrollToCorrectPosition() {
         if self.isFirstResponder {
             self.scrollRangeToVisible(NSMakeRange(-1, 0)) // Scroll to bottom
@@ -134,22 +105,19 @@ open class GrowingTextView: UITextView {
             self.scrollRangeToVisible(NSMakeRange(0, 0)) // Scroll to top
         }
     }
-    
     // Show placeholder if needed
     override open func draw(_ rect: CGRect) {
         super.draw(rect)
-        
         if text.isEmpty {
             let xValue = textContainerInset.left + textContainer.lineFragmentPadding
             let yValue = textContainerInset.top
             let width = rect.size.width - xValue - textContainerInset.right
             let height = rect.size.height - yValue - textContainerInset.bottom
             let placeholderRect = CGRect(x: xValue, y: yValue, width: width, height: height)
-            
             if let attributedPlaceholder = attributedPlaceholder {
                 // Prefer to use attributedPlaceholder
                 attributedPlaceholder.draw(in: placeholderRect)
-            } else if let placeholder = placeholder {
+            } else if let placeholder = customPlaceholder {
                 // Otherwise user placeholder and inherit `text` attributes
                 let paragraphStyle = NSMutableParagraphStyle()
                 paragraphStyle.alignment = textAlignment
@@ -160,12 +128,10 @@ open class GrowingTextView: UITextView {
                 if let font = font {
                     attributes[.font] = font
                 }
-                
                 placeholder.draw(in: placeholderRect, withAttributes: attributes)
             }
         }
     }
-    
     // Trim white space and new line characters when end editing.
     @objc func textDidEndEditing(notification: Notification) {
         if let sender = notification.object as? GrowingTextView, sender == self {
@@ -176,7 +142,6 @@ open class GrowingTextView: UITextView {
             scrollToCorrectPosition()
         }
     }
-    
     // Limit the length of text
     @objc func textDidChange(notification: Notification) {
         if let sender = notification.object as? GrowingTextView, sender == self {
